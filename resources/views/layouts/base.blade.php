6fe2b26ca3eae7b90720e091785e31828c4c242f
Guganp-8012 <gustavo.rios801208@gmail.com>
2024-11-14 11:31:52 -0300

    Movendo a navbar para layouts base

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,9 +4,45 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield('title', 'Título Padrão')</title>
-        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <body>
+        <ul class="nav justify-content-center" id="pills-tab" role="tablist" style="display: flex; width: 100%;">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('homepage') }}">Home</a>
+            </li>
+        
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Explorar+</a>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <a class="dropdown-item" href="{{ route('servico.index') }}">Serviços</a>
+                    <a class="dropdown-item" href="{{ route('empresa.sobre') }}">Nosso time</a>
+                    <a class="dropdown-item" href="#simulador">Simulador</a>
+                    <a class="dropdown-item" href="{{ route('FAQ.index') }}">Perguntas Frequentes</a>
+                    <a class="dropdown-item" href="{{ route('politica-privacidade.index') }}">Política de privacidade</a>
+                    <a class="dropdown-item" href="#">Página 404</a>  <!-- ## -->
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sobre Nós</a> <!-- empresa/sobre -->
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="#portifolio">Portfólio</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('blog.index') }}">Blog</a>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('empresa.contato') }} ">Contatos</a>
+            </li>
+        </ul>
+
+<!--adicionar o botão de login para todas as views-->
+    
         <div class="container">
             @yield('content')
         </div>
